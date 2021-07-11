2eb4a8a92d0157f5c324d5eec927e8f4f4dcc805
Cristian Sena <cristian.sena@extendeal.com>
2021-07-11 14:38:15 -0300

    Agrego funcionalidad

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Desafío: Array de productos
+
+### Ejemplo para guardar un producto por curl desde consola:
+
+```
+curl --location --request POST 'localhost:8080/api/productos' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+  "title": "Melón",
+  "price": 390,
+  "thumbnail": "http/localhost:8080/img/melon.jpg"
+}'
+```
--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 const puerto = 8080;
 
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 
-app.get("/", (req, res) => {    
-  res.send('Hola Mundo!');
+let productos = [];
+let producto;
+
+app.get("/api/productos", (req, res) => {
+  let response =
+    productos.length > 0 ? productos : { error: "no hay productos cargados" };
+  res.json(response);
+});
+
+app.get("/api/productos/:id", (req, res) => {
+  let paramId = req.params.id;
+  let producto = productos.filter(function (producto) {
+    return producto.id == paramId;
+  });
+
+  let response =
+    producto.length > 0 ? producto : { error: "producto no encontrado" };
+  res.send(response);
+});
+
+app.post("/api/productos", (req, res) => {
+  producto = req.body;
+  producto.id = productos.length + 1;
+  productos.push(req.body);
+
+  res.send(producto);
 });
 
 const server = app.listen(puerto, () => {
